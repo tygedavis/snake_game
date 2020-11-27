47bf9709ec77e5a90d893d82acb9ff90aecd8a24
tygedavis <tdavis@cmsonline.com>
2020-11-27 08:47:46 -0700

    Canvas initiated

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById('snakeGame');
+const ctx = canvas.getContext('2d');
+
+ctx.canvas.height = 800;
+ctx.canvas.width = 800;
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, 800, 800);
