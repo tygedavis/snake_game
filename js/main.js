1f2c3b7aac88fdf5d5d5ebb560c30c5d8655f531
tygedavis <tdavis@cmsonline.com>
2020-11-27 11:41:14 -0700

    Snake game works with basic functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Canvas ======================================================
 const canvas = document.getElementById('snakeGame');
 const ctx = canvas.getContext('2d');
 
@@ -6,6 +7,8 @@ ctx.canvas.width = 800;
 ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, 800, 800);
 
+
+// Variables ======================================================
 let snake = [
   { x: 200, y: 200 },
   { x: 190, y: 200 },
@@ -15,6 +18,8 @@ let snake = [
 let gameSpeed = 100;
 
 let ateFruit = false;
+
+let points = 0;
 
 function getRandom10(min, max) {
   return getRandomInt(min / 10, max / 10) * 10;
@@ -33,7 +38,7 @@ let fruit = {
 let dx = 10;
 let dy = 0;
 
-//Game
+// Game ======================================================
 update();
 
 function update() {
@@ -53,6 +58,7 @@ function gameOver() {
       { x: 190, y: 200 },
       { x: 180, y: 200 }
     ]
+    points = 0;
   }
 
   for (let i = 1; i < snake.length; i++){
@@ -64,11 +70,30 @@ function gameOver() {
       ];
       dx = 10;
       dy = 0;
+      points = 0;
     }
   }
 }
 
-//Snake
+function addPoints() {
+  if (snake.length < 10) {
+    points += 10
+  } else if (snake.length < 25) {
+    points += 25
+  } else if (snake.length < 50) {
+    points += 30
+  } else if (snake.length < 75) {
+    points += 40
+  } else if (snake.length < 100) {
+    points += 50
+  } else if (snake.length < 200) {
+    points += 75
+  } else if (snake.length > 200) {
+    points += 100
+  }
+}
+
+// Snake ======================================================
 function drawSnakeBody(body) {
   ctx.fillStyle = 'black';
   ctx.fillRect(body.x, body.y, 10, 10);
@@ -85,6 +110,8 @@ function moveSnake() {
     snake.pop();
   }
   ateFruit = false;
+
+  document.getElementById('pointCount').innerHTML = points;
 }
 
 setInterval(function onMoveSnake() {
@@ -114,7 +141,7 @@ function keyDown(e) {
 
 window.addEventListener('keydown', keyDown);
 
-//Fruit
+// Fruit ======================================================
 function drawFruit() {
   ctx.fillStyle = 'red';
   ctx.fillRect(fruit.x, fruit.y, 10, 10);
@@ -124,6 +151,7 @@ function eatFruit() {
   if (snake[0].x === fruit.x && snake[0].y === fruit.y) {
     fruit.x = getRandom10(0, 790)
     fruit.y = getRandom10(0, 790)
+    addPoints();
     ateFruit = true;
   }
 }
