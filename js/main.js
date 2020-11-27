699f7ef8ff35ea839d68f9fc060eec04075b51a0
tygedavis <tdavis@cmsonline.com>
2020-11-27 09:17:25 -0700

    Added snake character

--- js/main.js
+++ js/main.js
@@ -5,3 +5,30 @@ ctx.canvas.height = 800;
 ctx.canvas.width = 800;
 ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, 800, 800);
+
+let snake = [
+  {x:200, y: 200},
+  {x:190, y: 200},
+  {x:180, y: 200}
+];
+
+startGame();
+
+function startGame() {
+  clearCanvas();
+  drawSnake();
+};
+
+function clearCanvas() {
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, 800, 800);
+}
+
+function drawSnakeBody(body) {
+  ctx.fillStyle = 'black';
+  ctx.fillRect(body.x, body.y, 10, 10);
+}
+
+function drawSnake() {
+  snake.forEach(drawSnakeBody);
+};
