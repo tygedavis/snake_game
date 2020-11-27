96bd0874f4a743c50e972972ddac3bbc14d517c8
tygedavis <tdavis@cmsonline.com>
2020-11-27 09:58:42 -0700

    Snake can make movements based on keyboard input and initial fruit is generated

--- js/main.js
+++ js/main.js
@@ -7,23 +7,35 @@ ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, 800, 800);
 
 let snake = [
-  {x:200, y: 200},
-  {x:190, y: 200},
-  {x:180, y: 200}
+  { x: 200, y: 200 },
+  { x: 190, y: 200 },
+  { x: 180, y: 200 }
 ];
 
-startGame();
-
-function startGame() {
-  clearCanvas();
-  drawSnake();
-};
-
-function clearCanvas() {
-  ctx.fillStyle = 'white';
-  ctx.fillRect(0, 0, 800, 800);
+let fruit = {
+  x: 50,
+  y: 50
 }
 
+let gameSpeed = 10
+
+let dx = gameSpeed;
+let dy = 0;
+
+//Game
+update();
+
+function update() {
+  clearCanvas();
+  drawFruit();
+  drawSnake();
+}
+
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+//Snake
 function drawSnakeBody(body) {
   ctx.fillStyle = 'black';
   ctx.fillRect(body.x, body.y, 10, 10);
@@ -31,4 +43,42 @@ function drawSnakeBody(body) {
 
 function drawSnake() {
   snake.forEach(drawSnakeBody);
-};
+}
+
+function moveSnake() {
+  const head = { x: snake[0].x + dx, y: snake[0].y + dy };
+  snake.unshift(head);
+  snake.pop();
+}
+
+
+setInterval(function onMoveSnake() {
+  clearCanvas();
+  drawFruit();
+  moveSnake();
+  drawSnake();
+}, 1500);
+
+function keyDown(e) {
+  if (e.key === 'a') {
+    dy = 0;
+    dx = -gameSpeed;
+  } else if (e.key === 'd') {
+    dy = 0;
+    dx = gameSpeed;
+  } else if (e.key === 'w') {
+    dy = -gameSpeed;
+    dx = 0;
+  } else if (e.key === 's') {
+    dy = gameSpeed;
+    dx = 0;
+  }
+}
+
+window.addEventListener('keydown', keyDown);
+
+//Fruit
+function drawFruit() {
+  ctx.fillStyle = 'red';
+  ctx.fillRect(fruit.x, fruit.y, 10, 10);
+}
